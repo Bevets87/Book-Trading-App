42ecfe1fad88807b4bcba8f66fe88e891c0fbec0
Steve <bevets87@gmail.com>
2017-05-21 22:55:12 -0400

    fixed css on main page

--- src/server/config.js
+++ src/server/config.js
@@ -1,4 +1,4 @@
 export default {
-  JWT_SECRET: process.env.JWT_SECRET,
-  API_KEY: process.env.API_KEY
+  JWT_SECRET: process.env.JWT_SECRET || 'super secret secret of secrets',
+  API_KEY: process.env.API_KEY || 'pkXxiLL2NSejHsz051oEg'
 }
