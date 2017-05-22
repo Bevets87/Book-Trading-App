079cc0a4a60c48f6ffc708031166c2f265d6cc02
Steve <bevets87@gmail.com>
2017-05-21 22:05:30 -0400

    setup for prod

--- src/server/app.prod.js
+++ src/server/app.prod.js
@@ -4,6 +4,11 @@ import bodyParser from 'body-parser'
 import mongoose from 'mongoose'
 import compression from 'compression'
 
+import user from './routes/user'
+import api from './routes/api'
+import book from './routes/book'
+import request from './routes/request'
+
 let app = express()
 
 app.use(compression())
@@ -11,12 +16,16 @@ app.use(compression())
 app.use(express.static(path.join(__dirname, '../../dist' )))
 
 app.use(bodyParser.json())
+app.use('/', user)
+app.use('/api', api)
+app.use('/books', book)
+app.use('/trades', request)
 
 app.get('*', (req,res) => {
   res.sendFile(path.join(__dirname, '../../dist/index.html'))
 })
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/ReactStarterApp');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/BookTradingApp');
 
 mongoose.connection.once('open',function(){
   console.log('Connection has been made!')
